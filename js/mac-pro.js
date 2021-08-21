83b5e36a04ca5a10348cab30acf4628267b494c6
Ikraa <munnisaikra750@gmail.com>
2021-08-21 19:57:10 +0600

    promo code added

--- js/mac-pro.js
+++ js/mac-pro.js
@@ -1,87 +1,88 @@
-const bestPrice=document.getElementById('best-price');
-const memory8Gb=document.getElementById('btn-memory-8gb');
-const memory16Gb=document.getElementById('btn-memory-16gb');
-const memoryCost=document.getElementById('memory-cost');
-const storage256Gb=document.getElementById('ssd-256gb');
-const storage512Gb=document.getElementById('ssd-512gb');
-const storage1Tb= document.getElementById('ssd-1Tb');
-const storageCost=document.getElementById('storage-cost');
-const freeDelivery=document.getElementById('free')
-const deliveryCharge=document.getElementById('delivery-charge')
-const deliverySubTotal=document.getElementById('delivery-total')
-const totalPrice=document.getElementById('total-price');
-const buttonApply=document.getElementById('btn-apply');
-const promoCodeField= document.getElementById('promo-code-input');
-const grandTotal=document.getElementById('grand-total');
+// variable declaration:
+const bestPrice = document.getElementById('best-price');
+const memory8Gb = document.getElementById('btn-memory-8gb');
+const memory16Gb = document.getElementById('btn-memory-16gb');
+const memoryCost = document.getElementById('memory-cost');
+const storage256Gb = document.getElementById('ssd-256gb');
+const storage512Gb = document.getElementById('ssd-512gb');
+const storage1Tb = document.getElementById('ssd-1Tb');
+const storageCost = document.getElementById('storage-cost');
+const freeDelivery = document.getElementById('free')
+const deliveryCharge = document.getElementById('delivery-charge')
+const deliverySubTotal = document.getElementById('delivery-total')
+const totalPrice = document.getElementById('total-price');
+const buttonApply = document.getElementById('btn-apply');
+const grandTotal = document.getElementById('grand-total');
+
 
 // handle function of Total Price:(sub-total)
-function updateSubTotal()
-{   const bestPriceText=bestPrice.innerText;
-    const bestPriceProduct=parseFloat(bestPriceText);
-    const memoryCostText=memoryCost.innerText;
-    const memoryCostAll=parseFloat(memoryCostText);
-    const storageCostText=storageCost.innerText;
-    const storageCostAll=parseFloat(storageCostText);
-    const deliverySubText=deliverySubTotal.innerText;
-    const deliveryTotal=parseFloat(deliverySubText);
-    const subTotal=memoryCostAll+storageCostAll+deliveryTotal+bestPriceProduct;
-    totalPrice.innerText=subTotal;
-    const finalTotal=parseFloat(grandTotal.innerText);
-    grandTotal.innerText=finalTotal;
-    grandTotal.innerText=subTotal;
+function updateSubTotal() {
+    const bestPriceText = bestPrice.innerText;
+    const bestPriceProduct = parseFloat(bestPriceText);
+    const memoryCostText = memoryCost.innerText;
+    const memoryCostAll = parseFloat(memoryCostText);
+    const storageCostText = storageCost.innerText;
+    const storageCostAll = parseFloat(storageCostText);
+    const deliverySubText = deliverySubTotal.innerText;
+    const deliveryTotal = parseFloat(deliverySubText);
+    const subTotal = memoryCostAll + storageCostAll + deliveryTotal + bestPriceProduct;
+    totalPrice.innerText = subTotal;
+    let Total = parseFloat(grandTotal.innerText);
+    grandTotal.innerText = Total;
+    grandTotal.innerText = subTotal;
+    console.log(subTotal);
+
+   // promo(subTotal);
+    const promoCodee = document.getElementById('promo-code-input').value;
+    if (promoCodee == 'stevekaku') {
+        const total = parseFloat(subTotal);
+        // console.log('Before promo',total)
+        const afterPromoTotal = total - (total*0.20);
+        grandTotal.innerText=afterPromoTotal;
+        // console.log('After promo', parseInt(afterPromoTotal))
+    }
 };
+// console.log(promoCode);
 
-// handle function of Grand total price:(Total)
- function calculateTotal(){  
-    const promoCodeField= document.getElementById('promo-code-input');
-    const grandTotal=document.getElementById('grand-total');
-    promoCodeField.value='';
-    let promoCode=promoCodeField.value;
-    if(promoCode=="stevekaku"){
-        const discount=subTotal/20;
-        discount=promoCode+totalPrice.innerText;
-        grandTotal.innerText=discount;
-        return discount;
-     }
-}; 
-  
-  
+// handle function of Grand total price:(Total)---------------
 // handle event of Memory 8GB & 16GB :
-memory8Gb.addEventListener('click', function(){
-memoryCost.innerText='0';
-updateSubTotal()
-});
-memory16Gb.addEventListener('click', function(){
-    memoryCost.innerText="180";
+memory8Gb.addEventListener('click', function () {
+    memoryCost.innerText = '0';
     updateSubTotal()
 });
-
+memory16Gb.addEventListener('click', function () {
+    memoryCost.innerText = "180";
+    updateSubTotal()
+});
 // handle event of Storage 256GB:
-storage256Gb.addEventListener('click', function(){
-    storageCost.innerText='0';
+storage256Gb.addEventListener('click', function () {
+    storageCost.innerText = '0';
     updateSubTotal()
 });
 // handle event of Storage 512GB:
-storage512Gb.addEventListener('click', function(){
-    storageCost.innerText='100';
+storage512Gb.addEventListener('click', function () {
+    storageCost.innerText = '100';
     updateSubTotal()
 });
 // handle event of Storage 512GB:
-storage1Tb.addEventListener('click', function(){
-    storageCost.innerText='180';
+storage1Tb.addEventListener('click', function () {
+    storageCost.innerText = '180';
     updateSubTotal()
 });
 // handle event of delivery charge free & paid:
-freeDelivery.addEventListener('click', function(){
-    deliverySubTotal.innerText='0';
-    updateSubTotal();
-})
-deliveryCharge.addEventListener('click', function(){
-    deliverySubTotal.innerText='20';
+freeDelivery.addEventListener('click', function () {
+    deliverySubTotal.innerText = '0';
     updateSubTotal();
 });
-// handle event of promo code apply:
- 
-buttonApply.addEventListener('click', function(){
-    calculateTotal();
+deliveryCharge.addEventListener('click', function () {
+    deliverySubTotal.innerText = '20';
+    updateSubTotal();
 });
+// handle event and function of promo code apply:
+
+ buttonApply.addEventListener('click',function(){
+    promoCode();
+});
+function promoCode(){
+    updateSubTotal();
+}
